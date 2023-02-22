f8154048c3518f5a544654eb1e4eafb9a2ca5ba5
fran1204 <fmrodriguez@emergya.com>
2023-02-22 18:36:21 +0100

    Incluir login con google

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -52,7 +53,57 @@ const login = async(req, res = response) => {
         });
     }
 }
+
+const googleSignIn = async (req, res = response) => {
+    const { id_token } = req.body;
+
+    try {
+
+        const { nombre, img, correo } = await googleVerify(id_token);
+                
+        let usuario = await Usuario.findOne({ correo });
+            
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                password: '123456',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario(data);
+            
+            await usuario.save();
+        }
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: "Hable con el administrador, usuario bloqueado"
+            });
+        }
+
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+
+
+        res.json({
+            usuario,
+            token
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            error,
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+ }
+
  
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
